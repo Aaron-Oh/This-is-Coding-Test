ae0d030b45293a2d45b416ca94bfcdd73af01acf
Sung Chul Park <jasonsc@naver.com>
2024-02-27 07:45:38 +0900

    solve: 이코테 ch03-4

---
+++ This-is-Coding-Test/chap03-Greedy/3-4/chul_solution.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+cnt = 0
+while N != 1:
+    if N % K == 0:
+        N = N // K
+    else:
+        N -= 1
+    cnt += 1
+print(cnt)
